45d1de567b7512ccbf758bc8d4c0cad7f84ad226
MaxDistructo <MaxDistructo@users.noreply.github.com>
2022-07-04 19:30:57 -0500

    TerminalMenu: Setup template for the terminal menu test

--- TerminalMenu/src/main.cpp
+++ TerminalMenu/src/main.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include "Menu.h"
+
+void hello_world();
 
 int main()
+{
+    TerminalMenu menu;
+    menu.add_option("Say Hello World", hello_world);
+    menu.add_exit("Quit");
+    menu.set_continue_execute();
+    auto response = menu.execute();
+    return 0;
+}
+
+void hello_world()
 {
     printf("Hello World!\n");
 }
